e0aada999cac768067d519887fb2db23227d2554
Denis <loboda.deni@gmail.com>
2022-06-08 17:50:01 +0300

    Added default navigation component

--- core/src/commonMain/kotlin/com/idexoft/mvucore/api/INavigationComponent.kt
+++ core/src/commonMain/kotlin/com/idexoft/mvucore/api/INavigationComponent.kt
@@ -12,29 +12,42 @@ interface INavigationComponent {
 	fun observeForcePop(observer: (String)->Unit)
 
 	companion object {
-		fun empty() = object: INavigationComponent {
-			override fun replace(destination: String, args: Map<String, Any>?) {
-			}
-
-			override fun push(destination: String, args: Map<String, Any>?) {
-			}
-
-			override fun pop() {
-			}
+		fun default() = object: INavigationComponent {
+			private var replaceObserver: ((String, Map<String, Any>?) -> Unit)? = null
+			private var pushObserver: ((String, Map<String, Any>?) -> Unit)? = null
+			private var popObserver: (() -> Unit)? = null
+			private var forcePopObserver: ((String) -> Unit)? = null
 
 			override fun observeReplace(observer: (String, Map<String, Any>?) -> Unit) {
+				replaceObserver = observer
 			}
 
 			override fun observePush(observer: (String, Map<String, Any>?) -> Unit) {
+				pushObserver = observer
 			}
 
 			override fun observePop(observer: () -> Unit) {
+				popObserver = observer
+			}
+
+			override fun replace(destination: String, args: Map<String, Any>?) {
+				replaceObserver?.invoke(destination, args)
+			}
+
+			override fun push(destination: String, args: Map<String, Any>?) {
+				pushObserver?.invoke(destination, args)
+			}
+
+			override fun pop() {
+				popObserver?.invoke()
 			}
 
 			override fun forcePop(destination: String) {
+				forcePopObserver?.invoke(destination)
 			}
 
 			override fun observeForcePop(observer: (String) -> Unit) {
+				forcePopObserver = observer
 			}
 		}
 	}
